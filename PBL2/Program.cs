6cfe7c762f7bf26fc794b5e521d7a3f63b6f167d
HieuYeager <erenbeta3112@gmail.com>
2025-12-23 23:35:10 +0700

    test change path of file (BaoCaoPresenter)

--- PBL2/Program.cs
+++ PBL2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PBL2.Database;
 using PBL2.Views.staffView;
 
 namespace PBL2
@@ -15,10 +16,14 @@ namespace PBL2
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
 
+            string sql = NhanViens.select()
+                .From(NhanViens.TableName).Where($"{NhanViens.MaNV} = 'NV01'")
+                .GetSql();
+            MessageBox.Show(sql);
         }
     }
 }
