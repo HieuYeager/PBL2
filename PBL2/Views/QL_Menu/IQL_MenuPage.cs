cd451d809715106e34c50d16db2f0ffaa5e19caa
HieuYeager <erenbeta3112@gmail.com>
2025-12-25 12:07:48 +0700

    add cho co

---
+++ PBL2/Views/QL_Menu/IQL_MenuPage.cs
@@ -0,0 +1,16 @@
+﻿using PBL2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL2.Views.QL_Menu
+{
+    public interface IQL_MenuPage
+    {
+        void loadTable(List<Mon> mons);
+        void loadDetail(Mon mon);
+        void showMessage(string message);
+    }
+}
