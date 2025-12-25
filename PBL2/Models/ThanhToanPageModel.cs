2c32edd379fde518f7d1a17910577c296a9a91a8
HieuYeager <erenbeta3112@gmail.com>
2025-12-25 09:03:23 +0700

    xoa model du

--- PBL2/Models/ThanhToanPageModel.cs
+++ PBL2/Models/ThanhToanPageModel.cs
@@ -11,27 +11,6 @@ namespace PBL2.Models
 {
     public class ThanhToanPageModel: System.ComponentModel.INotifyPropertyChanged
     {
-        public OrderModel order { get; set; }
-
-        private decimal _TienThanhToan = 0;
-
-        public decimal TienThanhToan { 
-            get
-            {
-                return _TienThanhToan;
-            }
-            set 
-            {
-                _TienThanhToan = value;
-                TienThua = _TienThanhToan - order.Total;
-                OnPropertyChanged(nameof(TienThanhToan));
-                OnPropertyChanged(nameof(TienThua));
-            } 
-        }
-        public decimal TienThua { 
-            get => TienThanhToan - order.Total; 
-            set { } }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged(string propertyName)
